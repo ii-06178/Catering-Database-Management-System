eec6f014c1e07f86a45b2d51e9f35a0c85ec7e79
hr05940 <hr05940@st.habib.edu.pk>
2020-11-14 18:18:10 +0500

    Admin form design updated

--- CateringDatabaseSystem/Admin.Designer.cs
+++ CateringDatabaseSystem/Admin.Designer.cs
@@ -75,6 +75,7 @@
             this.groupBox1.Controls.Add(this.textBox3);
             this.groupBox1.Controls.Add(this.textBox2);
             this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Cera Pro", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(25, 25);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(373, 332);
@@ -91,6 +92,7 @@
             this.groupBox2.Controls.Add(this.textBox10);
             this.groupBox2.Controls.Add(this.label10);
             this.groupBox2.Controls.Add(this.textBox7);
+            this.groupBox2.Font = new System.Drawing.Font("Cera Pro", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox2.Location = new System.Drawing.Point(25, 363);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(373, 249);
@@ -105,9 +107,10 @@
             this.groupBox3.Controls.Add(this.textBox9);
             this.groupBox3.Controls.Add(this.label8);
             this.groupBox3.Controls.Add(this.textBox8);
+            this.groupBox3.Font = new System.Drawing.Font("Cera Pro", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox3.Location = new System.Drawing.Point(432, 25);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(454, 193);
+            this.groupBox3.Size = new System.Drawing.Size(472, 193);
             this.groupBox3.TabIndex = 1;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Remove Admin";
@@ -115,9 +118,10 @@
             // groupBox4
             // 
             this.groupBox4.Controls.Add(this.comboBox1);
+            this.groupBox4.Font = new System.Drawing.Font("Cera Pro", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox4.Location = new System.Drawing.Point(432, 255);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(454, 102);
+            this.groupBox4.Size = new System.Drawing.Size(472, 102);
             this.groupBox4.TabIndex = 1;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Update Database";
@@ -126,106 +130,114 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(137, 48);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(206, 26);
+            this.textBox1.Size = new System.Drawing.Size(206, 28);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(137, 94);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(206, 26);
+            this.textBox2.Size = new System.Drawing.Size(206, 28);
             this.textBox2.TabIndex = 1;
             // 
             // textBox3
             // 
             this.textBox3.Location = new System.Drawing.Point(137, 140);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(206, 26);
+            this.textBox3.Size = new System.Drawing.Size(206, 28);
             this.textBox3.TabIndex = 2;
             // 
             // textBox4
             // 
             this.textBox4.Location = new System.Drawing.Point(137, 186);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(206, 26);
+            this.textBox4.Size = new System.Drawing.Size(206, 28);
             this.textBox4.TabIndex = 3;
             // 
             // textBox5
             // 
             this.textBox5.Location = new System.Drawing.Point(137, 232);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(206, 26);
+            this.textBox5.Size = new System.Drawing.Size(206, 28);
             this.textBox5.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(35, 54);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 20);
+            this.label1.Size = new System.Drawing.Size(85, 22);
             this.label1.TabIndex = 5;
             this.label1.Text = "AdminID:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(35, 98);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.Size = new System.Drawing.Size(62, 22);
             this.label2.TabIndex = 6;
             this.label2.Text = "Name:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Montserrat", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(35, 142);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 20);
+            this.label3.Size = new System.Drawing.Size(58, 22);
             this.label3.TabIndex = 7;
             this.label3.Text = "Email:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Montserrat", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(35, 186);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(82, 20);
+            this.label4.Size = new System.Drawing.Size(90, 22);
             this.label4.TabIndex = 8;
             this.label4.Text = "Password:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Montserrat", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(35, 230);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(46, 20);
+            this.label5.Size = new System.Drawing.Size(49, 22);
             this.label5.TabIndex = 9;
             this.label5.Text = "Role:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Montserrat", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(35, 139);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(46, 20);
+            this.label6.Size = new System.Drawing.Size(49, 22);
             this.label6.TabIndex = 19;
             this.label6.Text = "Role:";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Montserrat", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(35, 95);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(82, 20);
+            this.label7.Size = new System.Drawing.Size(90, 22);
             this.label7.TabIndex = 18;
             this.label7.Text = "Password:";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Montserrat", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.Location = new System.Drawing.Point(35, 52);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(75, 20);
+            this.label10.Size = new System.Drawing.Size(85, 22);
             this.label10.TabIndex = 15;
             this.label10.Text = "AdminID:";
             // 
@@ -233,57 +245,64 @@
             // 
             this.textBox6.Location = new System.Drawing.Point(137, 141);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(206, 26);
+            this.textBox6.Size = new System.Drawing.Size(206, 28);
             this.textBox6.TabIndex = 14;
             // 
             // textBox7
             // 
             this.textBox7.Location = new System.Drawing.Point(137, 95);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(206, 26);
+            this.textBox7.Size = new System.Drawing.Size(206, 28);
             this.textBox7.TabIndex = 13;
             // 
             // textBox10
             // 
             this.textBox10.Location = new System.Drawing.Point(137, 46);
             this.textBox10.Name = "textBox10";
-            this.textBox10.Size = new System.Drawing.Size(206, 26);
+            this.textBox10.Size = new System.Drawing.Size(206, 28);
             this.textBox10.TabIndex = 10;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(40, 57);
+            this.label8.Font = new System.Drawing.Font("Montserrat", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(24, 56);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(75, 20);
+            this.label8.Size = new System.Drawing.Size(85, 22);
             this.label8.TabIndex = 11;
             this.label8.Text = "AdminID:";
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(219, 54);
+            this.textBox8.Font = new System.Drawing.Font("Montserrat", 8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox8.Location = new System.Drawing.Point(201, 54);
             this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(206, 26);
+            this.textBox8.Size = new System.Drawing.Size(247, 27);
             this.textBox8.TabIndex = 10;
+            this.textBox8.Text = "Enter your ID";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(40, 99);
+            this.label9.Font = new System.Drawing.Font("Montserrat", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(24, 98);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(152, 20);
+            this.label9.Size = new System.Drawing.Size(171, 22);
             this.label9.TabIndex = 13;
             this.label9.Text = "AdminIDToRemove:";
             // 
             // textBox9
             // 
-            this.textBox9.Location = new System.Drawing.Point(219, 95);
+            this.textBox9.Font = new System.Drawing.Font("Montserrat", 8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox9.Location = new System.Drawing.Point(201, 95);
             this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(206, 26);
+            this.textBox9.Size = new System.Drawing.Size(247, 27);
             this.textBox9.TabIndex = 12;
+            this.textBox9.Text = "Enter ID of Admin to remove";
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Montserrat", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(260, 273);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(83, 42);
@@ -293,6 +312,7 @@
             // 
             // button2
             // 
+            this.button2.Font = new System.Drawing.Font("Montserrat", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(258, 185);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(85, 42);
@@ -302,7 +322,8 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(335, 139);
+            this.button3.Font = new System.Drawing.Font("Montserrat", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(358, 140);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(90, 41);
             this.button3.TabIndex = 20;
@@ -311,14 +332,15 @@
             // 
             // comboBox1
             // 
+            this.comboBox1.Font = new System.Drawing.Font("Montserrat", 8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "Customers",
             "Orders",
             "Riders"});
-            this.comboBox1.Location = new System.Drawing.Point(44, 43);
+            this.comboBox1.Location = new System.Drawing.Point(28, 43);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(272, 28);
+            this.comboBox1.Size = new System.Drawing.Size(272, 30);
             this.comboBox1.TabIndex = 0;
             this.comboBox1.Text = "Select table to update";
             // 
@@ -326,6 +348,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.ClientSize = new System.Drawing.Size(970, 696);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox3);

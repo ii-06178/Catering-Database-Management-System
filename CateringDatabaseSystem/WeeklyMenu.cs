b0743ef2df2eb8aeda01ffa78d7f0be790413aef
hr05940 <hr05940@st.habib.edu.pk>
2020-11-14 20:05:44 +0500

    More forms added

--- CateringDatabaseSystem/WeeklyMenu.cs
+++ CateringDatabaseSystem/WeeklyMenu.cs
@@ -16,5 +16,10 @@ namespace CateringDatabaseSystem
         {
             InitializeComponent();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }

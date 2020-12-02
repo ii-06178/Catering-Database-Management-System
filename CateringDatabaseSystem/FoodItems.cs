c53eeaa3a17d6cc356c45c04fe91248b5d4b1b9a
hr05940 <hr05940@st.habib.edu.pk>
2020-12-03 00:54:22 +0500

    significant update to food items

--- CateringDatabaseSystem/FoodItems.cs
+++ CateringDatabaseSystem/FoodItems.cs
@@ -36,7 +36,7 @@ namespace CateringDatabaseSystem
             c.Inserts("insert into fooditem (fooditemID, itemName, Categories_CategoriesID, unitprice, unitquantity) values ((select max(fooditemID) from fooditem)+1 , '" + textBox2.Text + "', (select categoriesID from categories where categoryname = '" + comboBox2.Text + "'), " + textBox5.Text + ", " + textBox1.Text + ")");
             for (int i = 0; i < listView1.Items.Count; i++)
             {
-                c.Inserts("insert into ingredients_for_fooditem (fooditem_fooditemID, ingredients_ingredientsID, quantity) values ("+textBox1.Text + ",(select ingredientsid from ingredients where ingredientname = '" + listView1.Items[i].Text + "')," + listView2.Items[i].Text + ")");
+                c.Inserts("insert into ingredients_for_fooditem (fooditem_fooditemID, ingredients_ingredientsID, quantity_grams) values ((select max(fooditemID) from fooditem),(select ingredientsid from ingredients where ingredientname = '" + listView1.Items[i].Text + "')," + listView2.Items[i].Text + ")");
             }
             listView1.Clear();
             listView2.Clear();
@@ -52,21 +52,48 @@ namespace CateringDatabaseSystem
         private void button3_Click(object sender, EventArgs e)
         {//update food item
             ConnectingData c = new ConnectingData();
-            if (textBox3.Text != "")
-            {//updating unitprice
-                c.Inserts("update fooditem set unitprice = " + textBox3.Text + "where fooditemID = " + textBox7.Text);
+            if (textBox7.Text != "")
+            {//if ID not null
+                if (textBox3.Text != "")
+                {//updating unitprice
+                    c.Inserts("update fooditem set unitprice = " + textBox3.Text + "where fooditemID = " + textBox7.Text);
+                }
+                if (textBox8.Text != "")
+                {//updating item name
+                    c.Inserts("update fooditem set itemName = '" + textBox8.Text + "' where fooditemID = " + textBox7.Text);
+                }
+                if (comboBox3.Text != "")
+                {//updating item category
+                    c.Inserts("update fooditem set Categories_CategoriesID = (select categoriesID from categories where categoryname = '" + comboBox3.Text + "') where fooditemID = " + textBox7.Text);
+                }
+                if (textBox10.Text != "")
+                {//updating item name
+                    c.Inserts("update fooditem set unitQuantity = " + textBox10.Text + " where fooditemID = " + textBox7.Text);
+                }
+                if (comboBox4.Text != "" && comboBox4.Text != comboBox5.Text) //if ingredient to add and remove are diff
+                {//adding ingredient
+                    if (textBox11.Text != "")
+                    {//if quantity given
+                        c.Inserts("if not exists (select ingredients_ingredientsID from ingredients_for_fooditem where ingredients_ingredientsID = (select ingredientsid from ingredients where ingredientname = '" + comboBox4.Text + "')) begin insert into ingredients_for_fooditem (fooditem_fooditemID, ingredients_ingredientsID, quantity_grams) values (" + textBox7.Text + ",(select ingredientsid from ingredients where ingredientname = '" + comboBox4.Text + "')," + textBox11.Text + ") end");
+                        MessageBox.Show("Note: ingredient will only be added if it does not already exist. There will be no change otherwise.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Enter ingredient quantity (grams).");
+                    }
+                }
+                else if (comboBox4.Text != "" && comboBox4.Text == comboBox5.Text)
+                {
+                    MessageBox.Show("Cannot add and remove the same ingredient!");
+                }
+                if (comboBox5.Text != "")
+                {//removing ingredient
+                    c.Inserts("delete from ingredients_for_fooditem where ingredients_ingredientsID = (select ingredientsid from ingredients where ingredientname = '" + comboBox5.Text + "')");
+                }
             }
-            if (textBox8.Text != "")
-            {//updating item name
-                c.Inserts("update fooditem set itemName = '" + textBox8.Text + "' where fooditemID = " + textBox7.Text);
-            }
-            if (comboBox3.Text != "")
-            {//updating item category
-                c.Inserts("update fooditem set Categories_CategoriesID = (select categoriesID from categories where categoryname = '" + comboBox3.Text + "') where fooditemID = " + textBox7.Text);
-            }
-            if (textBox10.Text != "")
-            {//updating item name
-                c.Inserts("update fooditem set unitQuantity = " + textBox10.Text + " where fooditemID = " + textBox7.Text);
+            else
+            {
+                MessageBox.Show("Enter ID of ingredient to update!");
             }
         }
 
@@ -79,7 +106,7 @@ namespace CateringDatabaseSystem
         private void button7_Click(object sender, EventArgs e)
         {//show ingredients for selected food item
             ConnectingData c = new ConnectingData();
-            dataGridView1.DataSource = c.Select("select itemname as 'Food Item', ingredientname as 'Ingredients', Quantity from fooditem f inner join ingredients_for_fooditem fi on f.fooditemid = fi.fooditem_fooditemID inner join ingredients i on i.ingredientsid = fi.ingredients_ingredientsid where f.fooditemid = " + textBox9.Text + "order by itemname");
+            dataGridView1.DataSource = c.Select("select itemname as 'Food Item', ingredientname as 'Ingredients', Quantity_grams as 'Quantity (grams)' from fooditem f inner join ingredients_for_fooditem fi on f.fooditemid = fi.fooditem_fooditemID inner join ingredients i on i.ingredientsid = fi.ingredients_ingredientsid where f.fooditemid = " + textBox9.Text + "order by itemname");
         }
 
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
@@ -101,25 +128,46 @@ namespace CateringDatabaseSystem
             row[0] = 0;
             row[1] = "";
             ds.Rows.InsertAt(row, 0);
-
             comboBox2.DataSource = ds;
             comboBox2.DisplayMember = "CategoryName";
             comboBox2.ValueMember = "CategoriesID";
-            comboBox3.DataSource = ds;
+
+            DataTable ds5 = c.Select("Select CategoriesID, CategoryName from Categories");
+            DataRow row5 = ds5.NewRow(); //adding default null value as first element in combobox
+            row5[0] = 0;
+            row5[1] = "";
+            ds5.Rows.InsertAt(row5, 0);
+            comboBox3.DataSource = ds5;
             comboBox3.DisplayMember = "CategoryName";
             comboBox3.ValueMember = "CategoriesID";
 
             //populating ingredients combobox with values from the database (categories table)
-            ConnectingData c2 = new ConnectingData();
-            DataTable ds2 = c2.Select("Select IngredientsID, IngredientName from Ingredients");
+            DataTable ds2 = c.Select("Select IngredientsID, IngredientName from Ingredients");
             DataRow row2 = ds2.NewRow(); //adding default null value as first element in combobox
             row2[0] = 0;
             row2[1] = "";
             ds2.Rows.InsertAt(row2, 0);
-
             comboBox1.DataSource = ds2;
             comboBox1.DisplayMember = "IngredientName";
             comboBox1.ValueMember = "IngredientsID";
+
+            DataTable ds3 = c.Select("Select IngredientsID, IngredientName from Ingredients");
+            DataRow row3 = ds3.NewRow(); //adding default null value as first element in combobox
+            row3[0] = 0;
+            row3[1] = "";
+            ds3.Rows.InsertAt(row3, 0);
+            comboBox5.DataSource = ds3;
+            comboBox5.DisplayMember = "IngredientName";
+            comboBox5.ValueMember = "IngredientsID";
+
+            DataTable ds4 = c.Select("Select IngredientsID, IngredientName from Ingredients");
+            DataRow row4 = ds4.NewRow(); //adding default null value as first element in combobox
+            row4[0] = 0;
+            row4[1] = "";
+            ds4.Rows.InsertAt(row4, 0);
+            comboBox4.DataSource = ds4;
+            comboBox4.DisplayMember = "IngredientName";
+            comboBox4.ValueMember = "IngredientsID";
         }
     }
 }
